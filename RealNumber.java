1f4def4a7f1fc01a87ebb67de488fe2719de6133
Kenli8504 <kli22@stuy.edu>
2020-11-29 02:55:41 -0500

    I think this is good? i moved the equals to Number

--- RealNumber.java
+++ RealNumber.java
@@ -1,4 +1,4 @@
-public class RealNumber{
+public class RealNumber extends Number{
   private double value;
 
   public RealNumber(double v){
@@ -12,55 +12,22 @@ public class RealNumber{
   public String toString(){
     return ""+getValue();
   }
-  //---------ONLY EDIT BELOW THIS LINE------------
 
-  /*
-  *Return true when the values are within 0.001% of eachother.
-  *Special case: if one is exactly zero, the other must be exactly zero.
-  */
-  public boolean equals(RealNumber other){
-    if (this.getValue() == 0){
-      if (other.getValue() == 0){
-        return true;
-      }
-    }
-    if ( (Math.abs(this.getValue()*1.00001) >= Math.abs(other.getValue())) && (Math.abs(this.getValue()*.99999) <= Math.abs(other.getValue())) ){
-      return true;
-    }
-    return false;
-  }
-
-  /*
-  *Return a new RealNumber that has the value of:
-  *the sum of this and the other
-  */
   public RealNumber add(RealNumber other){
     RealNumber sum = new RealNumber (this.getValue() + other.getValue());
     return sum;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *the product of this and the other
-  */
   public RealNumber multiply(RealNumber other){
     RealNumber product = new RealNumber (this.getValue() * other.getValue());
     return product;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this divided by the other
-  */
   public RealNumber divide(RealNumber other){
     RealNumber quotient = new RealNumber (this.getValue() / other.getValue());
     return quotient;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this minus the other
-  */
   public RealNumber subtract(RealNumber other){
     RealNumber difference = new RealNumber (this.getValue() - other.getValue());
     return difference;
