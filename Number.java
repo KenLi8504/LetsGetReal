c0b25a77e9b95b112a698ece931e49c0f3e6667f
Kenli8504 <kli22@stuy.edu>
2020-11-29 00:35:45 -0500

    Trying to figure out problem with equals method

---
+++ Number.java
@@ -0,0 +1,25 @@
+public abstract class Number{
+  public abstract double getValue();
+
+  public boolean equals(RealNumber other){
+    if (this.getValue() == 0){
+      if (other.getValue() == 0){
+        return true;
+      }
+    }
+    if ( (Math.abs(this.getValue()*1.00001) >= Math.abs(other.getValue())) && (Math.abs(this.getValue()*.99999) <= Math.abs(other.getValue())) ){
+      return true;
+    }
+    return false;
+  }
+
+  public int compareTo (Number other){
+    if (this.equals(other) ) {
+      return 0;
+    }
+    else if (this.getValue() < other.getValue() ){
+      return  -1;
+    }
+    return 1;
+  }
+}
